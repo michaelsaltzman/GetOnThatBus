a0c3f19f9122f3dad737180383a3a6f30d3b79e4
michaelsaltzman <michaelasaltzman@gmail.com>
2016-02-02 12:20:00 -0800

    Initial Commit

---
+++ GetOnThatBus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetOnThatBus
+//
+//  Created by Michael Saltzman on 2/2/16.
+//  Copyright © 2016 Michael Saltzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
